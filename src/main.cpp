abc9a835428817cd178f9442b64d6b5bb889b05f
Andrii Andriichuk <myanddriychuk@gmail.com>
2022-09-15 19:50:45 +0300

    add mmap usage for file reading

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,30 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <cstdio>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <string_view>
+
+static const size_t MAX_LETTER_COUNT = 50;
+
+void nullTheWord(char* word, size_t bound)
+{
+	for(int i = 0; i <= bound; ++i)
+	{
+		word[i] = '\0';
+	}
+}
 
 int main(int argc, const char* argv[])
 {
 	struct stat status;
 	size_t size = -1;
+	std::set<std::string> words;
+	char* word = new char[MAX_LETTER_COUNT];
+	char* pc=word;
+
 	if (argc != 2)
 	{
 		std::cout << "error: bad arguments!" << std::endl;
@@ -20,22 +39,39 @@ int main(int argc, const char* argv[])
 	// get file size
 	fstat(fd, &status);
 	size = status.st_size;
-	size_t portion = size / 2;
-	const char* mapped = nullptr;
-	mapped = reinterpret_cast<char*>(mmap(mapped, portion, PROT_READ, MAP_PRIVATE, fd, 0));
-	std::cout << "First part: " << std::endl;
-	for (int i = 0; i < size; ++i)
-	{
-		char c = mapped[i];
-		putchar(c);
-	}
-	std::cout << "Second part" << std::endl;
 
-	mapped = reinterpret_cast<char*>(mmap(mapped, size-portion, PROT_READ, MAP_PRIVATE, fd, portion));
-	for (int i = 0; i < size; ++i)
+	size_t portion = size / 2;
+	char* mapped = nullptr;
+	mapped = reinterpret_cast<char*>(mmap(mapped, 10, PROT_READ, MAP_PRIVATE, fd, 0));
+	//std::string_view myStr(mapped, size); 
+	//std::cout << "string view: \n" << myStr << std::endl;
+	std::cout << "First part: " << std::endl;
+
+	for (int i = 0; i < 10; ++i)
 	{
 		char c = mapped[i];
 		putchar(c);
+		/*if(c != ' ')
+		{
+			*pc++ = c;
+		}
+		else
+		{
+			words.insert({word});
+			nullTheWord(word, pc-word);
+			pc = word;
+		}*/
 	}
+	//words.insert({word});
+	std::cout << "\nSecond part" << std::endl;
+	char * new_mapped = reinterpret_cast<char*>(mmap(mapped, 10, PROT_READ, MAP_PRIVATE, fd, 1));
+	for (int i = 0; i < 10; ++i)
+	{
+		char c = new_mapped[i];
+		putchar(c);
+	}
+	std::cout << std::endl;
+	std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(std::cout, " "));
+	std::cout << "\nSize of set: " << words.size() << std::endl;
 	return 0;
 }
