ef76629fcd1f57b807bf5ce01b6ff3a6320cc94d
Andrii Andriichuk <myanddriychuk@gmail.com>
2022-09-16 18:44:33 +0300

    refactor code: change function names

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,18 @@
 #include <thread>
 #include <unistd.h>
 
-static const size_t MAX_LETTER_COUNT = 50;
+#include "threadsafe_queue.h"
+
 static const int BUFFER_SIZE = sysconf(_SC_PAGE_SIZE); 
 
-void nullTheWord(char* word, size_t bound)
-{
-	for(int i = 0; i <= bound; ++i)
-	{
-		word[i] = '\0';
-	}
-}
 class FileReader
 {
 public:
 	FileReader(const char* fName);
 	~FileReader();
 	bool initialize();
+	void readFile();
+	void readChunk(int fd, int i);
 	struct Chunk
 	{
 		std::set<std::string> words;
@@ -35,14 +31,13 @@ public:
 		std::string lastWord;
 		int inst;
 	};
-	void readChunks();
-	void handleChunk(int fd, int i);
 private:
 	const char* fileName;
 	int fd;
 	size_t fileSize = -1;
 	std::vector<std::thread> threads;
 	int numOfChunks;
+	ThreadSafeQueue<Chunk> chunks;
 };
 FileReader::FileReader(const char* fName)
 {
@@ -73,18 +68,19 @@ bool FileReader::initialize()
 	if(fileSize % BUFFER_SIZE) numOfChunks++;
 	return true;
 }
-void FileReader::readChunks()
+void FileReader::readFile()
 {
 	for(int i = 0; i < numOfChunks; ++i)
 	{
-		threads.emplace_back(&FileReader::handleChunk, this, fd, i);
+		threads.emplace_back(&FileReader::readChunk, this, fd, i);
 	}
 }
-void FileReader::handleChunk(int fd, int i)
+void FileReader::readChunk(int fd, int i)
 {
 	char* mapped = reinterpret_cast<char*>(mmap(nullptr, BUFFER_SIZE, PROT_READ, MAP_PRIVATE, fd, i*BUFFER_SIZE));
-	std::string_view myStr(mapped, BUFFER_SIZE); 
-	std::cout << "string view number " << i << ": " << myStr << std::endl;
+	std::string_view stringChunk(mapped, BUFFER_SIZE); 
+	//std::cout << "string view number " << i << ": " << myStr << std::endl;
+	
 	munmap(mapped, BUFFER_SIZE); 
 }
 int main(int argc, const char* argv[])
@@ -100,7 +96,7 @@ int main(int argc, const char* argv[])
 		std::cout << "ERROR: could not initialize data!" << std::endl;
 		return -1;
 	}
-	fileReader.readChunks();
+	fileReader.readFile();
 	
 	//std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(std::cout, " "));
 
